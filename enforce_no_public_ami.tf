922ddf01db61c999972b6dfc6cc866855fd82a0c
rjulian <richard@rjulian.net>
2020-02-17 14:19:45 -0800

    Patch: pin to release of engine code

--- enforce_no_public_ami.tf
+++ enforce_no_public_ami.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "enforce_no_public_ami" {
-  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda"
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda?ref=v0.0.1"
   rule_name        = "EnforceNoPublicAMI"
   rule_description = "Rule to check if AMI is modified to be public"
 
